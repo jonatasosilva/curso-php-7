11335b4ed512612cf3a842d18d4fe9f3a8d57133
Jonatas Silva <jonatasosilva@protonmail.com>
2020-07-13 19:54:13 -0300

    funções e data

---
+++ date/exemplo-01.php
@@ -0,0 +1,9 @@
+<?php
+
+echo date("d/m/Y H:i:s", 1594679128);
+
+echo "<br>";
+
+echo time();
+
+?>
---
+++ funcoes/exemplo-10.php
@@ -0,0 +1,11 @@
+<?php
+
+function test($callback) {
+    $callback();
+}
+
+test(function() {
+    echo "Terminou!";
+});
+
+?>
